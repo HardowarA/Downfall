37338fca7f0e9eb0f9c0df81d6485deeb034d494
ahardowar <aaron.hardowar@stonybrook.edu>
2018-07-29 17:42:57 -0400

    Scroll and Platform Functionality added

--- downfall/downfall.js
+++ downfall/downfall.js
@@ -105,6 +105,7 @@ this.canvas = document.getElementById("downfallCanvas");
   let balldy = 5;
   let ballrad = 10;
   let ballDisplacement = 103;
+  let lowestPoint = 390;
 
   let maxlinkdx = 500;
   let linkplatformdx = 0;
@@ -182,23 +183,92 @@ this.canvas = document.getElementById("downfallCanvas");
   function linkedPlatform(height, linkplatformx, linkplatformy) {
     const platformArray = [];
 
+
+    let leastWidthCoverage = 400;
+    let greatestWidthCoverage = 450;
+
+    let targetWidthCoverage = (greatestWidthCoverage - Math.floor(Math.random()*50));
+    let currentWidthCoverage = 0;
+    let maxdeathSpaces = 5 - Math.floor(Math.random()* 2);
+    let maxdeathSpaceCoverage = 200;
+    let currentDeathSpaces = 0;
+    let currentDeathSpaceCoverage = 0;
+
+    function doesPlatformConflict(plat) {
+      debugger
+      if (platformArray.length === 0) {
+        return true;
+      }
+      let doesIt = true;
+      for(let i = 0; i < platformArray.length; i++) {
+        let platform = platformArray[i];
+        debugger
+        if( !((platform.platformx + platform.platformlength < plat.platformx) &&
+           (platform.platformx + platform.platformlength > plat.platformx + plat.platformlength) &&
+           (platform.platformx < plat.platformx) &&
+           (platform.platformx > plat.platformx + plat.platformlength) )) {
+             doesIt = false;
+        }
+      }
+      return doesIt;
+    }
+    debugger
+    while(currentWidthCoverage < targetWidthCoverage) {
+      debugger
+      let platWidth = Math.floor(Math.random()*501);
+      let platLength = 150 - Math.floor(Math.random()*100);
+      let colorArray = ['red', 'green'];
+      let colorChoice = 1;
+      if (maxdeathSpaces > currentDeathSpaces && maxdeathSpaceCoverage > currentDeathSpaceCoverage) {
+        colorChoice = Math.floor(Math.random()*2);
+      }
+      let platColor = colorArray[colorChoice];
+      let plat = new Platform(platWidth, height, platLength, platColor);
+      debugger
+      // doesPlatformConflict(plat);
+      // while(doesPlatformConflict(plat) !== true) {
+      //   debugger
+      //  platWidth = 150 - Math.floor(Math.random()*100);
+      //  platLength = 150 - Math.floor(Math.random()*100);
+      //  colorArray = ['red', 'green'];
+      //  if (maxdeathSpaces < currentDeathSpaces && maxdeathSpaceCoverage < currentDeathSpaceCoverage) {
+      //    colorChoice = Math.floor(Math.random()*2);
+      //  } else {
+      //    colorChoice = 1;
+      //  }
+      //  platColor = colorArray[colorChoice];
+      //  plat = new Platform(platWidth, height, platLength, platColor);
+      //  debugger
+      // }
+      platformArray.push(plat);
+      let platLeft = new Platform(platWidth - 500, height, platLength, platColor);
+      let platRight = new Platform(platWidth + 500, height, platLength, platColor);
+      platformArray.push(platLeft);
+      platformArray.push(platRight);
+      currentWidthCoverage += platLength;
+      if(platColor === 'red') {
+        currentDeathSpaces += 1;
+        currentDeathSpaceCoverage += platLength;
+      }
+    }
+
     // platformArray.push(new Platform(-500, 400, 100, "#eee"));
-    platformArray.push(new Platform(-400, height, 100, "red"));
-    platformArray.push(new Platform(-300, height, 100, "blue"));
-    // platformArray.push(new Platform(-200, 400, 100, "#eee"));
-    platformArray.push(new Platform(-100, height, 100, "green"));
-
-    // platformArray.push(new Platform(0, 400, 100, "#eee"));
-    platformArray.push(new Platform(100, height, 100, "red"));
-    platformArray.push(new Platform(200, height, 100, "blue"));
-    // platformArray.push(new Platform(300, 400, 100, "#eee"));
-    platformArray.push(new Platform(400, height, 100, "green"));
-
-    // platformArray.push(new Platform(500, 400, 100, "#eee"));
-    platformArray.push(new Platform(600, height, 100, "red"));
-    platformArray.push(new Platform(700, height, 100, "blue"));
-    // platformArray.push(new Platform(800, 400, 100, "#eee"));
-    platformArray.push(new Platform(900, height, 100, "green"));
+    // platformArray.push(new Platform(-400, height, 100, "red"));
+    // platformArray.push(new Platform(-300, height, 100, "blue"));
+    // // platformArray.push(new Platform(-200, 400, 100, "#eee"));
+    // platformArray.push(new Platform(-100, height, 100, "green"));
+    //
+    // // platformArray.push(new Platform(0, 400, 100, "#eee"));
+    // platformArray.push(new Platform(100, height, 100, "red"));
+    // platformArray.push(new Platform(200, height, 100, "blue"));
+    // // platformArray.push(new Platform(300, 400, 100, "#eee"));
+    // platformArray.push(new Platform(400, height, 100, "green"));
+    //
+    // // platformArray.push(new Platform(500, 400, 100, "#eee"));
+    // platformArray.push(new Platform(600, height, 100, "red"));
+    // platformArray.push(new Platform(700, height, 100, "blue"));
+    // // platformArray.push(new Platform(800, 400, 100, "#eee"));
+    // platformArray.push(new Platform(900, height, 100, "green"));
 
     let linkedplatform = new LinkedPlatform(platformArray);
     return linkedplatform;
@@ -207,10 +277,10 @@ this.canvas = document.getElementById("downfallCanvas");
   function levels(linkplatformx, linkplatformy) {
     let linkedplatforms = [];
     let height = 400;
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 100; i++) {
       let linkedplatform = linkedPlatform(height);
       linkedplatforms.push(linkedplatform);
-      height += 100;
+      height += 200;
     }
     let levelBuilder = new LevelBuilder(linkedplatforms);
     return levelBuilder;
@@ -246,13 +316,102 @@ this.canvas = document.getElementById("downfallCanvas");
     }
   }
 
+  let count = 0;
+  let dropCounter = 0;
+
   function animate() {
+    let ballBottom = bally + ballrad;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    platformDetection();
-    maxHeightDetection();
+    let pD = platformDetection();
+    // platformDetection();
+    // let count = 200;
+    if(pD) {
+      count = 1;
+      // dropCounter = 0;
+    }
+    // if(dropCounter > 0) {
+    //   count = 1;
+    // }
+    // if(!pD) {
+    //   dropCounter = 1;
+    // }
+    // let initialPlatforms = levelsBuilder.linkedplatforms.length;
+    if(ballBottom === 405 && !pD) {
+      // dropCounter = 1;
+      // debugger
+    //   if(count === 1) {
+    //     debugger
+    //   levelsBuilder.linkedplatforms[0].status = 0;
+    //   levelsBuilder.linkedplatforms.shift();
+    //   count -= 1;
+    // }
+      // debugger
+      // debugger
+      // ballDisplacement = 103;
+      let countball = 100;
+      if (countball !== 0) {
+        bally += balldy
+        countball -= balldy;
+      }
+      let countPlat = 100;
+      if(countPlat !== 0) {
+        linkplatformdy -= 5;
+        countPlat -= 5;
+      }
+      // debugger
+      if(levelsBuilder.linkedplatforms[0].platforms[0].platformy + linkplatformdy < 400) {
+        // debugger
+        levelsBuilder.linkedplatforms[0].status = 0;
+        levelsBuilder.linkedplatforms.shift();
+      }
 
-    
+      // if(dropCounter > 0) {
+      //   debugger
+      //   dropCounter = 0;
+      //   count = 1;
+      // }
+      // let countBoth
+      // animatePlatform(count);
+      // balldy = 5;
+      // debugger
+      // cancelAnimationFrame(id);
+    }
+    // if(levelsBuilder.linkedplatforms.length !== initialPlatforms && pD) {
+    //   debugger
+    //   count = 1;
+    // }
+
+    maxHeightDetection();
+    // let count = 100;
+    // console.log(!platformDetection());
+      // debugger
+
+    //   balldy = 0;
+    //   animatePlatform(count);
+    //   balldy = 5;
+    //   // let id = animatePlatform(count);
+    //   // cancelAnimationFrame(id);
+    //   //   while(true){
+    //   //     // debugger
+    //   //   if(count === 0) {
+    //   //     break;
+    //   //   }
+    //   //   // debugger
+    //   //   linkplatformdy -= 1;
+    //   //   bally -= 2;
+    //   //   drawBall();
+    //   //   platformDetection();
+    //   //   maxHeightDetection();
+    //   //   levelsBuilder.renderLevel(linkplatformdx, linkplatformdy);
+    //   //   count -= 5;
+    //   //   // debugger
+    //   //   console.log('moving');
+    //   // }
+    // }
+
+
 
     levelsBuilder.renderLevel(linkplatformdx, linkplatformdy);
     if(rightPressed) {
@@ -261,6 +420,13 @@ this.canvas = document.getElementById("downfallCanvas");
          linkplatformdx -= 8;
        }
     bally -= balldy;
+    // debugger
+    ballBottom = bally + ballrad
+    // if(ballBottom > levelsBuilder.linkedplatforms[0].platforms[0].platformy + linkplatformdy + 6) {
+    //   linkplatformdy -= 100;
+    //   bally -= 200;
+    //   // debugger
+    // }
       //   platformDetection();
       //   maxHeightDetection();
       //   if(rightPressed && platformx < canvas.width - platformlength) {
@@ -277,45 +443,129 @@ this.canvas = document.getElementById("downfallCanvas");
     requestAnimationFrame(animate);
   }
 
+  function animatePlatform(count) {
+    let id = requestAnimationFrame(animatePlatform);
+    // debugger
+    while(true){
+      // debugger
+    if(count === 0) {
+      cancelAnimationFrame(id);
+      // balldy = 5;
+      break;
+    }
+    // debugger
+    linkplatformdy -= 100;
+
+    // balldy = 0;
+    // return id;
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // drawBall();
+    // platformDetection();
+    // maxHeightDetection();
+    // levelsBuilder.renderLevel(linkplatformdx, linkplatformdy);
+    count -= 200;
+    // debugger
+    console.log('moving');
+  }
+  }
+
   function platformDetection() {
-    ballLeft = ballx - ballrad;
-    ballRight = ballx + ballrad;
-    ballBottom = bally + ballrad
+    let ballLeft = ballx - ballrad;
+    let ballRight = ballx + ballrad;
+    let ballBottom = bally + ballrad;
+    // const initballBottom = ballBottom;
     // debugger ;
     // debugger
     let allLevels = levelsBuilder.linkedplatforms;
-    let linkedplatform = allLevels[0];
+    // let linkedplatform = allLevels[0];
+    // debugger
+
+    // let toggle = false;
+    for(let s = 0; s < allLevels.length; s++) {
+      let linkedplatform = allLevels[s];
     if(linkedplatform.status === 1) {
       let platforms = linkedplatform.platforms;
       for(let i = 0; i < platforms.length; i++){
         platform = platforms[i];
         let platformx = platform.platformx + linkplatformdx;
+        let platformy = platform.platformy + linkplatformdy;
         // let platformx = Math.abs(platform.platformx);
         // debugger
+        // debugger
+        // debugger
+
+        // if (!( (ballLeft >= platformx && ballLeft <= platformx +
+        // platform.platformlength && ballBottom - 3 >= platformy) ||
+        // (ballRight >= platformx && ballRight <= platformx +
+        //   platform.platformlength && ballBottom -3 >= platformy))) {
+        //     debugger
+        //   console.log('what');  // debugger
+        // }
+
+        // if(!((ballLeft >= platformx || ballLeft <= platformx +
+        // platform.platformlength) ||
+        // (ballRight >= platformx || ballRight <= platformx +
+        //   platform.platformlength))) {
+        //     debugger
+        //   }
+
         if( (ballLeft >= platformx && ballLeft <= platformx +
-        platform.platformlength && ballBottom - 3 >= platform.platformy) ||
+        platform.platformlength && ballBottom - 3 >= platformy) ||
         (ballRight >= platformx && ballRight <= platformx +
-          platform.platformlength && ballBottom -3 >= platform.platformy)) {
+          platform.platformlength && ballBottom -3 >= platformy)) {
             // debugger
           // (ballRight > platformx + linkplatformdx && ballRight < platformx +
             // platform.platformlength + linkplatformdx && ballBottom > platform.platformy + linkplatformdy)) {
           balldy = -balldy;
           ballDisplacement = 0;
-          break;
+          // toggle = true;
+          // debugger
+          return true;
+
           // debugger
         // } else if(ballBottom > platform.platformy) {
         //   debugger
         //   linkedplatform.status = 0;
-      } else if(ballBottom >= platform.platformy + 6) {
-          // debugger
-          linkedplatform.status = 0;
-          allLevels.shift();
-          break;
-          // debugger
-        }
+      // }
+    }// else if ( (ballBottom === 405 ) && !((ballLeft >= platformx && ballLeft <= platformx +
+    // platform.platformlength && ballBottom - 3 >= platformy) ||
+    // (ballRight >= platformx && ballRight <= platformx +
+    //   platform.platformlength && ballBottom -3 >= platformy))) {
+    //       debugger
+    //       let d= 0;
+    //       d+=1;
+    //       console.log(d);
+    //       linkedplatform.status = 0;
+    //       allLevels.shift();
+    //       // let id = animatePlatformUp();
+    //       // if(id) {
+    //       //   cancelAnimationFrame(id);
+    //       // }
+    //
+    //       // linkplatformdy -= 100;
+    //
+    //       // bally -= 100;
+    //       // levelsBuilder.renderLevel(linkplatformdx, linkplatformdy);
+    //
+    //       return false
+    //       // debugger
+    //     }
       }
     }
   }
+    return false;
+  }
+
+  // function animatePlatformUp() {
+  //   let count = 100;
+  //   while(count > 0) {
+  //     linkplatformdy += 5;
+  //     count -= 5;
+  //   }
+  //   levelsBuilder.renderLevel(linkplatformdx, linkplatformdy);
+  //
+  //   return requestAnimationFrame(animatePlatformUp);
+  // }
 
   function maxHeightDetection() {
     if(ballDisplacement >= 103) {
